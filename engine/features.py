2881727de18d0d26b0837757a2399e6bf2764bdd
Soumyadip-Dhara <soumyadipdhara7@gmail.com>
2025-06-01 20:50:31 +0530

    added the function that plays the start up sound

---
+++ engine/features.py
@@ -0,0 +1,7 @@
+from playsound import playsound
+
+
+# Playing assistant sound function
+def playAssistantSound():
+    music_dir="www\\assets\\audio\\soft-startup-sound-269291.mp3"
+    playsound(music_dir)
--- www/main.py
+++ www/main.py
@@ -1,11 +1,21 @@
+import sys
 import os
 
 import eel
 
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from engine.features import *
+
 eel.init("www")
 
+playAssistantSound()
+
 os.system('start msedge.exe --app="http://localhost:8000/index.html"')
+
+
 
 eel.start('index.html', mode=None, 
         #    size=(800, 600), position=(100, 100), port=8000,
            host='localhost', block=True)
+
