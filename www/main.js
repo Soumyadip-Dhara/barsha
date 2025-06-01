35570e39039fce8a16c795d8cba7e6abe11ca509
Soumyadip-Dhara <soumyadipdhara7@gmail.com>
2025-06-01 20:50:03 +0530

    added the configuration for siri waves

--- www/main.js
+++ www/main.js
@@ -7,6 +7,32 @@ $(document).ready(function () {
         },
         out:{
             effect: "bounceOut"
-        }
-    })
+        },
+    });
+
+    //Siri wave configuration
+    var siriWave = new SiriWave({
+    container: document.getElementById("siri-container"),
+    width: 800,
+    height: 200,
+    style: "ios9",
+    speed: 0.05,
+    amplitude: 1,
+    autostart: true,
+  });
+
+  //Siri message animation
+
+  $('.siri-message').textillate({
+        loop: true,
+        sync: true,
+        in:{
+            effect: "fadeInUp",
+            sync: true,
+        },
+        out:{
+            effect: "fadeOutUp",
+            sync: true,
+        },
+    });
 });
